ccdbc895447c1a1719c7de9fd2c19ab70e742cd7
Alireza <alireza.kariminejad@yahoo.com>
2023-01-09 23:11:48 +0330

    Add column of containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calculator_application/constants/constants.dart';
 
 void main() {
   runApp(const Application());
@@ -15,10 +16,29 @@ class _ApplicationState extends State<Application> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'gilroy'),
       home: Scaffold(
+        backgroundColor: backgroundGrayDark,
         body: SafeArea(
-          child: Text('alireza'),
-        ),
+            child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                height: 150.0,
+                color: backgroundGrayDark,
+              ),
+            ),
+            Expanded(
+              flex: 7,
+              child: Container(
+                height: 150.0,
+                color: backgroundGray,
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
