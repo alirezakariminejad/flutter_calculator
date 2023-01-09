2e7922a898657eb367492da7efbd086d8fc0b2e9
Alireza <alireza.kariminejad@yahoo.com>
2023-01-09 23:05:14 +0330

    Add constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color textGreen = HexColor('#38b387');
+final Color textGray = HexColor('#93a59e');
+final Color backgroundGray = HexColor('#2e3951');
+final Color backgroundGrayDark = HexColor('#212b41');
